2e884d20e9e4d61671ba7982b1eced376e3a011e
Loren Sands-Ramshaw <lorensr@gmail.com>
2011-08-04 23:44:42 -0400

    removed compiled stuff from vc

--- extension/compiled/background.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-    return sendResponse({
-      value: localStorage[request.key]
-    });
-  });
-}).call(this);
--- extension/compiled/init.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  window.f = {};
-  f.open = function(url) {
-    return chrome.tabs.create({
-      url: url
-    });
-  };
-}).call(this);
